fcc42ee1ea4d7643940d2fa76c859b1345d75d5d
Mathias <email@example.com>
2024-07-12 08:13:00 +0000

    add

--- examples/selenium_example.rs
+++ examples/selenium_example.rs
@@ -8,7 +8,7 @@
 //! Run as follows:
 //!
 //!     cargo run --example selenium_example
-
+use log::info;
 use thirtyfour::prelude::*;
 
 #[tokio::main]
@@ -17,13 +17,15 @@ async fn main() -> color_eyre::Result<()> {
     // it much easier to locate where the error occurred.
     color_eyre::install()?;
 
-    let caps = DesiredCapabilities::chrome();
+    // let caps = DesiredCapabilities::chrome();
     // NOTE: For selenium 3.x, use "http://localhost:4444/wd/hub/session".
-    let driver = WebDriver::new("http://localhost:4444", caps).await?;
+    // let driver = WebDriver::new("http://localhost:4444", caps).await?;
+
+    let _driver = initialize_driver().await?;
 
     // Navigate to https://wikipedia.org.
-    driver.goto("https://wikipedia.org").await?;
-    let elem_form = driver.find(By::Id("search-form")).await?;
+    _driver.goto("https://wikipedia.org").await?;
+    let elem_form = _driver.find(By::Id("search-form")).await?;
 
     // Find element from element.
     let elem_text = elem_form.find(By::Id("searchInput")).await?;
@@ -36,11 +38,37 @@ async fn main() -> color_eyre::Result<()> {
     elem_button.click().await?;
 
     // Look for header to implicitly wait for the page to load.
-    driver.find(By::ClassName("firstHeading")).await?;
-    assert_eq!(driver.title().await?, "Selenium - Wikipedia");
+    _driver.find(By::ClassName("firstHeading")).await?;
+    assert_eq!(_driver.title().await?, "Selenium - Wikipedia");
 
     // Always explicitly close the browser. There are no async destructors.
-    driver.quit().await?;
+    _driver.quit().await?;
 
     Ok(())
+}
+
+async fn initialize_driver() -> Result<WebDriver, WebDriverError> {
+    info!("initialize_driver - start");
+
+    let mut _caps = DesiredCapabilities::chrome();
+
+    // let mut caps: thirtyfour::ChromeCapabilities = DesiredCapabilities::chrome();
+    // caps.add_chrome_arg("--headless")?;
+    // caps.add_chrome_arg("--no-sandbox")?;
+    //  caps.add_chrome_arg("--disable-dev-shm-usage")?;
+
+    _caps.add_arg("--remote-debugging-pipe")?;
+    _caps.add_arg("--no-sandbox")?;
+    
+    let driver_result = WebDriver::new("http://localhost:9515", _caps).await;
+
+    // let result = WebDriver::new("http://localhost:4444/wd/hub", &caps).await;
+    let driver = match driver_result {
+        Ok(value) => value,
+        Err(error) => return Err(error),
+    };
+
+    driver.maximize_window().await?;
+    info!("initialize_driver - end");
+    Ok(driver)
 }
